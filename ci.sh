7004e911ff1aba02d7b4eaceb8f9cb7f18ea4872
Andrew Jones <andrew@andrewjones.id.au>
2015-11-30 09:38:15 +1100

    tidy up ci.sh

--- ci.sh
+++ ci.sh
@@ -1,9 +1,12 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
 HUGO_VERSION=0.14
+S3_BUCKET=cryptohack.net
+
 wget https://github.com/spf13/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_linux_amd64.tar.gz
 tar xvzf hugo_${HUGO_VERSION}_linux_amd64.tar.gz
 cp hugo_${HUGO_VERSION}_linux_amd64/hugo_${HUGO_VERSION}_linux_amd64 hugo
 
 ./hugo -v -t hugo-zen
 
-aws s3 sync public/ s3://cryptohack.net --acl public-read --region ap-southeast-2 --delete
+aws s3 sync public/ s3://${S3_BUCKET} --acl public-read --region ap-southeast-2 --delete
